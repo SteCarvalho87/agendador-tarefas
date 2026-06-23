e816ea479abb48533386d3e3ff1b6682453be534
Stephanie <ste.aoc@gmail.com>
2026-06-23 09:20:39 -0300

    adicionadas configurações de seguranca

---
+++ src/main/java/com/stephanie/agendadortarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.stephanie.agendadortarefas.infrastructure.security;
+
+import com.stephanie.agendadortarefas.business.dto.UsuarioDTO;
+import com.stephanie.agendadortarefas.infrastructure.security.Client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    @Autowired
+    private UsuarioClient client;
+
+       public UserDetails carrregaDadosUsuario(String email, String token){
+
+        UsuarioDTO usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build(); // Constrói o objeto UserDetails
+    }
+}
